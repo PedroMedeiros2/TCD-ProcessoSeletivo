87d44daebc03b7493b4186427a9e929498a08d76
Pedro Henrique Soares Medeiros <111461491+PedroMedeiros2@users.noreply.github.com>
2023-11-17 16:06:34 -0300

    init

---
+++ src/main/java/com/ph_sm/tcdprocessoseletivo/entities/users/Admin.java
@@ -0,0 +1,9 @@
+package com.ph_sm.tcdprocessoseletivo.entities.users;
+
+import com.ph_sm.tcdprocessoseletivo.entities.person.Person;
+
+public class Admin {
+    private Long id;
+    private Credential credential;
+    private Person person;
+}
